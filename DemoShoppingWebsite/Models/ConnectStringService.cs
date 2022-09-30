bc2b3a59dbe4203456eaeeed700b22c857a1b57e
正釗 陳 <Tony_Chen@otsukael.com.tw>
2022-09-30 15:22:04 +0800

    Add EncryptService

--- DemoShoppingWebsite/Models/ConnectStringService.cs
+++ DemoShoppingWebsite/Models/ConnectStringService.cs
@@ -10,9 +10,8 @@ namespace DemoShoppingWebsite.Models
     {
         public static dbShoppingCarAzureEntities CreateDBContext()
         {
-            var myEncrypt = new MyEncrypt();
             var db_ConnStr = ConfigurationManager.AppSettings["db_ConnStr"];
-            var db_Pwd = myEncrypt.Decrypt(ConfigurationManager.AppSettings["db_Pwd"]);
+            var db_Pwd = EncryptService.DecryptBase64(ConfigurationManager.AppSettings["db_Pwd"]);
 
             var db_ConnStrFull = $@"{db_ConnStr}password={db_Pwd}""";
 
---
+++ DemoShoppingWebsite/Models/EncryptService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace DemoShoppingWebsite.Models
+{
+    public class EncryptService
+    {
+        static string keystr = "01234567";
+        static string ivstr = "abcdefgh";
+
+        public static string EncryptBase64(string SourceStr)
+        {
+            string encrypt = string.Empty;
+
+            AesCryptoServiceProvider aes = new AesCryptoServiceProvider();
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            SHA256CryptoServiceProvider sha256 = new SHA256CryptoServiceProvider();
+            byte[] key = sha256.ComputeHash(Encoding.UTF8.GetBytes(keystr));
+            byte[] iv = md5.ComputeHash(Encoding.UTF8.GetBytes(ivstr));
+            aes.Key = key;
+            aes.IV = iv;
+
+            byte[] dataByteArray = Encoding.UTF8.GetBytes(SourceStr);
+            using (MemoryStream ms = new MemoryStream())
+            using (CryptoStream cs = new CryptoStream(ms, aes.CreateEncryptor(), CryptoStreamMode.Write))
+            {
+                cs.Write(dataByteArray, 0, dataByteArray.Length);
+                cs.FlushFinalBlock();
+                encrypt = Convert.ToBase64String(ms.ToArray());
+            }
+            return encrypt;
+        }
+
+        public static string DecryptBase64(string EncryptStr)
+        {
+            string decrypt = string.Empty;
+            AesCryptoServiceProvider aes = new AesCryptoServiceProvider();
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            SHA256CryptoServiceProvider sha256 = new SHA256CryptoServiceProvider();
+            byte[] key = sha256.ComputeHash(Encoding.UTF8.GetBytes(keystr));
+            byte[] iv = md5.ComputeHash(Encoding.UTF8.GetBytes(ivstr));
+            aes.Key = key;
+            aes.IV = iv;
+            byte[] dataByteArray = Convert.FromBase64String(EncryptStr);
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                {
+                    cs.Write(dataByteArray, 0, dataByteArray.Length);
+                    cs.FlushFinalBlock();
+                    decrypt = Encoding.UTF8.GetString(ms.ToArray());
+                }
+            }
+            return decrypt;
+        }
+    }
+}
